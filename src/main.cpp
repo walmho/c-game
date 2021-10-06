7594f46198a8a12b840ca7fa3943ed5451d9cb3f
walmho <jones.ryan.t.01@gmail.com>
2021-10-05 23:01:36 -0700

    Code now generates a random number

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,18 @@
 //Test Code - first real project!
 //This program is a random number guesser
 
-//Including libraries 
-#include "test.h"
+//Including libraries
+#include <time.h>
 #include <iostream>
 #include <stdlib.h> //srand number generator
 
 int main() {
     //Main code goes here
-    srand(time(NULL));
+    srand (time(NULL));
     int randNumber = rand() % 100+1;
     int guess = 0;
 
-    std::cout << number << end1; //debug?
+    std::cout << randNumber;
 
-    return 0; //Let's compiler know whether the program was a success or not
+    return 0; //Lets compiler know whether the program was a success or not
 }
