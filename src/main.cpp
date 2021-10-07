5cff761b67f81ee8c8ed6a03dd9322cfc86cbf95
walmho <jones.ryan.t.01@gmail.com>
2021-10-06 17:40:59 -0700

    Finished up random number guesser. Added logic and do while loop

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,27 @@
 
 int main() {
     //Main code goes here
-    generateNumber();
-    // srand (time(NULL));
-    // int randNumber = rand() % 100+1;
-    // int guess = 0;
+    srand (time(NULL));
+    int randNumber = rand() % 100+1;
+    int guess = 0;
 
-    // std::cout << randNumber;
+    std::cout << randNumber; // Prints out random number. Take out later once game is done
 
-    return 0; //Lets compiler know whether the program was a success or not
+do {
+    while (randNumber != guess) {
+        std::cout << "Guess a number between 1 and 100: ";
+        std::cin >> guess;
+        if (guess < randNumber) {
+            std::cout << "Guess Higher!\n" << std::endl;
+            }
+        else if (guess > randNumber) {
+            std::cout << "Guess Lower!\n" << std::endl;
+            }
+        else {
+            std::cout << "You guessed the Number!" << std::endl;
+            exit(0);
+            }
+    }
+} while (randNumber != guess );
+return 0; //Lets compiler know whether the program was a success or not
 }
